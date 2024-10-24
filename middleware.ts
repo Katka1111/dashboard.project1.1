7467856569027e6aaab68844644eef9882786400
Katka1111 <ksnopkova@novis.eu>
2024-10-24 12:43:15 +0200

    loginv10

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,14 @@
 import { NextResponse } from 'next/server';
-import { clerkMiddleware, ClerkMiddlewareAuth } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import type { NextRequest } from 'next/server';
 
 // Define protected routes
-const protectedRoutes = ['/dashboard-project1-1.vercel.app(.*)'];
+const protectedRoutes = createRouteMatcher(['/dashboard(.*)']);
 
-export default clerkMiddleware((auth: ClerkMiddlewareAuth, req: NextRequest) => {
-  const isPublicRoute = !protectedRoutes.some(route => req.nextUrl.pathname.match(route));
-
-  if (isPublicRoute) {
-    return NextResponse.next();
-  }
-
-  // Handle authenticated requests
-  if (auth.session) {
-    return NextResponse.next();
-  }
-
-  // Handle non-authenticated requests to protected routes
-  if (!auth.session && protectedRoutes.some(route => req.nextUrl.pathname.match(route))) {
-    const signInUrl = new URL('/sign-in', req.url);
-    signInUrl.searchParams.set('redirect_url', req.url);
-    return NextResponse.redirect(signInUrl);
+export default clerkMiddleware((auth, req: NextRequest) => {
+  if (protectedRoutes(req)) {
+    // Protect dashboard routes
+    return auth().protect();
   }
 
   // For all other cases, continue with the request
@@ -29,5 +16,10 @@ export default clerkMiddleware((auth: ClerkMiddlewareAuth, req: NextRequest) => 
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
