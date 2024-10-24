2bd9008a4c4811663be0215cb1a906b719c4c282
Katka1111 <ksnopkova@novis.eu>
2024-10-24 10:45:25 +0200

    loginv2

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { clerkMiddleware, createRouteMatcher, getAuth } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import type { NextRequest } from 'next/server';
 
 
@@ -7,11 +7,12 @@ const isProtectedRoute = createRouteMatcher([
     '/dashboard-project1-1.vercel.app(.*)'
 ]);
 export default clerkMiddleware((auth, req) => {
-  const userId = auth.userId;
   if (isProtectedRoute(req)) {
-    auth.protect();
+    return auth.protect();
   }
+
   // You can add custom logic here based on the user's authentication status
+  const { userId } = auth;
   if (!userId) {
     // If the user is not authenticated, you can redirect them or return a response
     // For example:
