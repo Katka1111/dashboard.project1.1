9bbbe830474c8a4e3af7c934eed17bf29cb148e8
Katka1111 <ksnopkova@novis.eu>
2024-10-24 12:55:16 +0200

    loginv14

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,9 @@
-import { NextResponse } from 'next/server';
-import { clerkMiddleware, createRouteMatcher, auth } from "@clerk/nextjs/server";
-import type { NextRequest } from 'next/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-// Define protected routes
-const protectedRoutes = createRouteMatcher(['/dashboard(.*)']);
-
-export default clerkMiddleware(async (_, req: NextRequest) => {
-  if (protectedRoutes(req)) {
-    // Protect dashboard routes
-    const { userId } = await auth();
-    if (!userId) {
-      const signInUrl = new URL('/sign-in', req.url);
-      signInUrl.searchParams.set('redirect_url', req.url);
-      return NextResponse.redirect(signInUrl);
-    }
-  }
-
-  // For all other cases, continue with the request
-  return NextResponse.next();
+export default authMiddleware({
+  publicRoutes: ["/", "/api/public"],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
