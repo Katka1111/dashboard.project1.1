e2bdd573af83d5fb2906575df0f65546748703f3
Katka1111 <ksnopkova@novis.eu>
2024-10-24 12:59:30 +0200

    loginv15

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,20 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/api/public"],
+export default clerkMiddleware((auth, req) => {
+  const publicRoutes = ["/", "/api/public"];
+  
+  if (publicRoutes.includes(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  if (!auth.userId) {
+    const signInUrl = new URL('/sign-in', req.url);
+    signInUrl.searchParams.set('redirect_url', req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
